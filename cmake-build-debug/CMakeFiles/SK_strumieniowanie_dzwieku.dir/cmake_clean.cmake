2ec987b3199235a2ce3a7adf8a0e61fbbabf6737
ikami <chicken3579@o2.pl>
2017-11-24 12:40:51 +0100

    initial project version

---
+++ cmake-build-debug/CMakeFiles/SK_strumieniowanie_dzwieku.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SK_strumieniowanie_dzwieku.dir/main.cpp.o"
+  "SK_strumieniowanie_dzwieku.pdb"
+  "SK_strumieniowanie_dzwieku"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SK_strumieniowanie_dzwieku.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/SK_strumieniowanie_dzwieku.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SK_strumieniowanie_dzwieku.
+# This may be replaced when dependencies are built.
