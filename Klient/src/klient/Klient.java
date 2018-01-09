9df8fcab95b88308563e521b222789ce4c3fb306
TheRealAven <wojciech.obst@student.put.poznan.pl>
2018-01-09 19:20:14 +0100

    Odtwarzanie dzwieku z bufora

--- Klient/src/klient/Klient.java
+++ Klient/src/klient/Klient.java
@@ -5,17 +5,53 @@
  */
 package klient;
 
+import java.io.*;
+import java.io.InputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.logging.*;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.SourceDataLine;
+
 /**
  *
  * @author Aven
  */
 public class Klient {
-
+    private final static int BUFBYTESIZE = 1;
+    private final static int PORT=90000;
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            Socket sock = new Socket("127.0.0.1",PORT);//Potencjalnie zmienić String na InetAddress
+            
+            InputStream is = sock.getInputStream();
+            
+            //init bufora na audiostream
+            final AudioFormat format = new AudioFormat(44100,8,1,true,true);//Definicja formatu audio: SampleRate, SampleSizeInBits, channels, signed, bigEndian,
+            final DataLine.Info info = new DataLine.Info(SourceDataLine.class, format, 1);
+            final SourceDataLine soundLine = (SourceDataLine) AudioSystem.getLine(info);
+            
+            int bufSize=2200;
+            soundLine.open(format, bufSize);
+            soundLine.start();
+            
+            byte[] buf=new byte[BUFBYTESIZE];
+            while(true){
+                int len = is.read(buf);
+                if(len == -1) break;
+                soundLine.write(buf, 0, bufSize);
+            }
+            
+            sock.close();
+        } catch (Exception ex) {
+            Logger.getLogger(Klient.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
