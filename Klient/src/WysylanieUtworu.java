7c2bf4cc7968a684d4ffdd6a2ea1182de6aed6c1
travis-ci <travis@travis-ci.org>
2018-02-12 22:27:10 +0100

    Praktycznie gotowy klient po spotkaniu 12.02

---
+++ Klient/src/WysylanieUtworu.java
@@ -0,0 +1,66 @@
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Aven
+ */
+public class WysylanieUtworu implements Runnable{
+    private final static int PORT=41112;
+    private String pathname;
+    
+    public WysylanieUtworu(String p){
+        pathname=p;
+    }
+    
+    @Override
+    public void run() {
+        try {
+            Socket sock = new Socket("192.168.2.54",PORT);
+            OutputStream os = sock.getOutputStream();
+            DataOutputStream ds= new DataOutputStream(os);
+            File plik;
+            synchronized(pathname){
+                plik=new File(pathname);
+            }
+            Long l=plik.length();
+            byte plikBuf[]=new byte[(int)plik.length()];
+            int licz;
+            String s="1";
+            FileInputStream fis = new FileInputStream(plik);
+            BufferedInputStream in = new BufferedInputStream(fis);
+            os.write(s.getBytes("UTF-8"));
+            ds.writeInt((int)plik.length());
+            os.write(plik.getName().getBytes("UTF-8"));
+            while((licz=in.read(plikBuf))>0){
+                os.write(plikBuf);
+            }
+            sock.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    public void setPath(String p){
+        synchronized(pathname){
+            pathname=p;
+        }
+    }
+}
